634043a7059ef935a733e1e1d2ff5adad94e86fc
Michał Paduch (INI) <INI@kmd.dk>
2021-11-17 11:19:37 +0100

    Add base layout

---
+++ MPTimer/MPTimerWeb/ClientApp/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,50 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, combineLatest, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidebarComponent implements OnInit {
+
+  private expand$ = new BehaviorSubject<boolean>(true);
+  private items$: Observable<SidebarItem[]> = of([
+    {
+      label: 'Dashboard',
+      icon: 'fa-chart-line',
+      link: '',
+    },
+    {
+      label: 'Work Tasks',
+      icon: 'fa-clipboard-list',
+      link: 'work-tasks',
+    },
+  ]);
+
+  public vm$ = combineLatest([
+    this.expand$,
+    this.items$,
+  ]).pipe(
+    map(([expanded, items]) => ({
+      expanded, items,
+    })),
+  );
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public toggleExpand(): void {
+    this.expand$.next(!this.expand$.value);
+  }
+}
+
+class SidebarItem {
+  public label!: string;
+  public icon!: string;
+  public link!: string;
+}
